f688bc09428bcd57e5a01b40aaa8cbcf65c9c5e3
yang <itheima@itcast.cn>
2022-09-06 11:21:32 +0800

    comment:添加了注释

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
 <script>
 // git log 查看commit记录
 // git status 查看的仓库的状态
-// git add .
+// git add . 提交到暂存区
+// git commit -m ''
+// feat:新增特性 style:样式的修改 fixed:debug comment:添加了注释
 export default {
   data () {
     return {}
